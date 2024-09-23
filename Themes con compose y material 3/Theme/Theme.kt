093fb84202fd7639a89371876a393ae6d0a0a05f
UlisesBG <98203741+UlisesBGZ@users.noreply.github.com>
2024-09-22 23:51:31 -0600

    Create Theme.kt

---
+++ Themes con compose y material 3/Theme/Theme.kt
@@ -0,0 +1,35 @@
+
+package com.example.reply.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = Purple40,
+    secondary = PurpleGrey40,
+    tertiary = Pink40
+
+)
+
+@Composable
+fun ReplyTheme(
+    darkTheme: Boolean = false,
+    content: @Composable () -> Unit
+) {
+    val replyColorScheme = when {
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+    MaterialTheme(
+        colorScheme = replyColorScheme,
+        content = content
+    )
+}
